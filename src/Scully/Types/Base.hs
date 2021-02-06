eae7acf4993e2185f71d4c29591ea0bf6b6239de
Deniz A. Durmaz <denizalpd@ogr.iu.edu.tr>
2021-02-06 22:33:28 +0300

    Add the remaining set of types we need to represent API responses
    This should be all there is based on the API documentation but even
    if it isn't we're handling unknown values (where asked by the documentation)
    by falling back on values specified or those that make sense.

--- src/Scully/Types/Base.hs
+++ src/Scully/Types/Base.hs
@@ -4,7 +4,6 @@
 module Scully.Types.Base
   ( Base,
     Bases,
-
     BaseId,
 
     -- * Lenses, Isos and Prisms
@@ -34,12 +33,14 @@ newtype BaseName = BaseName Text
 ------------------------------------------------------------------------------
 
 -- | Sum type defining the possible different levels of access.
+-- API consumers are expected to handle unknown permission levels
+-- gracefully by falling back to read behavior.
 data BasePermissionLevel
   = BasePermissionLevel_Read
   | BasePermissionLevel_Comment
   | BasePermissionLevel_Edit
   | BasePermissionLevel_Create
-  | BasePermissionLevel_Unknown
+
 
 -- | Iso to go between BasePermissionLevel and Text.
 basePermissionLevelText :: Iso' BasePermissionLevel Text
@@ -50,19 +51,14 @@ basePermissionLevelText = iso toText fromText
       BasePermissionLevel_Comment -> "comment"
       BasePermissionLevel_Edit -> "edit"
       BasePermissionLevel_Create -> "create"
-      BasePermissionLevel_Unknown -> "read"
-      -- ^ API consumers are expected to handle unknown permission levels
-      -- gracefully by falling back to read behavior.
     fromText = \case
       "read" -> BasePermissionLevel_Read
       "comment" -> BasePermissionLevel_Comment
       "edit" -> BasePermissionLevel_Edit
       "create" -> BasePermissionLevel_Create
       _ -> BasePermissionLevel_Read
-      -- ^ API consumers are expected to handle unknown permission levels
-      -- gracefully by falling back to read behavior.
 
--- TODO(dalp): Use the iso to get this instance for free.
+-- TODO(dalp): Create an iso and use that to get this instance.
 instance FromJSON BasePermissionLevel where
   parseJSON = pure . \case
     "read" -> BasePermissionLevel_Read
@@ -82,11 +78,10 @@ data Base = Base
 
 instance FromJSON Base where
   parseJSON = A.withObject "Base" $ \o -> do
-    id' <- o A..: "id"
-    name <- o A..: "name"
-    permissionLevel <- o A..: "permissionLevel"
-    pure $
-      Base id' name permissionLevel
+    _base_id <- o A..: "id"
+    _base_name <- o A..: "name"
+    _base_permissionLevel <- o A..: "permissionLevel"
+    pure $ Base {..}
 
 ------------------------------------------------------------------------------
 
@@ -95,6 +90,5 @@ newtype Bases = Bases [Base]
 
 instance FromJSON Bases where
   parseJSON = A.withObject "Bases" $ \o -> do
-    bases <- o A..: "bases"
-    pure $
-      Bases bases
+    baseList <- o A..: "bases"
+    pure $ Bases baseList
--- src/Scully/Types/Table.hs
+++ src/Scully/Types/Table.hs
@@ -28,92 +28,187 @@ newtype TableName = TableName Text
 ------------------------------------------------------------------------------
 
 -- | Newtype wrapper for a table's primary field identifier.
-newtype TablePrimaryFieldId = TablePrimaryFieldId Text
+newtype PrimaryFieldId = PrimaryFieldId Text
   deriving newtype FromJSON
 
 ------------------------------------------------------------------------------
 
 -- | Newtype wrapper for a table's fields' identifiers.
-newtype TableFieldId =  TableFieldId Text
+newtype FieldId =  FieldId Text
   deriving newtype FromJSON
 
 ------------------------------------------------------------------------------
 
 -- | Newtype wrapper for a table's fields' names.
-newtype TableFieldName = TableFieldName Text
+newtype FieldName = FieldName Text
   deriving newtype FromJSON
 
 ------------------------------------------------------------------------------
 
--- |
-data TableFieldType
-  = TableFieldType_AutoNumber
-  | TableFieldType_Unknown
+-- | API consumers are expected to handle unknown field types gracefully but
+-- API doesn't specify what value to fall back on, my money is on
+-- `FieldType_MultilineText`.
+data FieldType
+  = FieldType_AutoNumber
+  | FieldType_Barcode
+  | FieldType_Button
+  | FieldType_Checkbox
+  | FieldType_Count
+  | FieldType_CreatedTime
+  | FieldType_Currency
+  | FieldType_Date
+  | FieldType_DateTime
+  | FieldType_Duration
+  | FieldType_Email
+  | FieldType_Formula
+  | FieldType_LastModifiedBy
+  | FieldType_LastModifiedTime
+  | FieldType_MultilineText
+  | FieldType_MultipleAttachments
+  | FieldType_MultipleCollaborators
+  | FieldType_MultipleLookupValues
+  | FieldType_MultipleRecordLinks
+  | FieldType_MultipleSelects
+  | FieldType_Number
+  | FieldType_Percent
+  | FieldType_PhoneNumber
+  | FieldType_Rating
+  | FieldType_RichText
+  | FieldType_Rollup
+  | FieldType_SingleCollaborator
+  | FieldType_SingleLineText
+  | FieldType_SingleSelect
+  | FieldType_Url
 
--- TODO(dalp): Create an iso and use that to get this instance for free.
-instance FromJSON TableFieldType where
+instance FromJSON FieldType where
   parseJSON = pure . \case
-    "autoNumber" -> TableFieldType_AutoNumber
-    _ -> TableFieldType_Unknown
-    
--- autoNumber
--- barcode
--- button
--- checkbox
--- count
--- createdBy
--- createdTime
--- currency
--- date
--- dateTime
--- duration
--- email
--- formula
--- lastModifiedBy
--- lastModifiedTime
--- multilineText
--- multipleAttachments
--- multipleCollaborators
--- multipleLookupValues
--- multipleRecordLinks
--- multipleSelects
--- number
--- percent
--- phoneNumber
--- rating
--- richText
--- rollup
--- singleCollaborator
--- singleLineText
--- singleSelect
--- url
- 
+    "autoNumber" -> FieldType_AutoNumber
+    "barcode" -> FieldType_Barcode
+    "button" -> FieldType_Button
+    "checkbox" -> FieldType_Checkbox
+    "count" -> FieldType_Count
+    "createdTime" -> FieldType_CreatedTime
+    "currency" -> FieldType_Currency
+    "date" -> FieldType_Date
+    "dateTime" -> FieldType_DateTime
+    "duration" -> FieldType_Duration
+    "email" -> FieldType_Email
+    "formula" -> FieldType_Formula
+    "lastModifiedBy" -> FieldType_LastModifiedBy
+    "lastModifiedTime" -> FieldType_LastModifiedTime
+    "multilineText" -> FieldType_MultilineText
+    "multipleAttachments" -> FieldType_MultipleAttachments
+    "multipleCollaborators" -> FieldType_MultipleCollaborators
+    "multipleLookupValues" -> FieldType_MultipleLookupValues
+    "multipleRecordLinks" -> FieldType_MultipleRecordLinks
+    "multipleSelects" -> FieldType_MultipleSelects
+    "number" -> FieldType_Number
+    "percent" -> FieldType_Percent
+    "phoneNumber" -> FieldType_PhoneNumber
+    "rating" -> FieldType_Rating
+    "richText" -> FieldType_RichText
+    "rollup" -> FieldType_Rollup
+    "singleCollaborator" -> FieldType_SingleCollaborator
+    "singleLineText" -> FieldType_SingleLineText
+    "singleSelect" -> FieldType_SingleSelect
+    "url" -> FieldType_Url
+    _ -> FieldType_MultilineText
+
 ------------------------------------------------------------------------------
 
 -- | 
-data TableField = TableField
-  { _tableField_id :: TableFieldId,
-    _tableField_name :: TableFieldName,
-    _tableField_type :: TableFieldType
+data Field = Field
+  { _field_id :: FieldId,
+    _field_name :: FieldName,
+    _field_type :: FieldType
   }
 
-instance FromJSON TableField where
-  parseJSON = A.withObject "TableField" $ \o -> do
-    id' <- o A..: "id"
-    name <- o A..: "name"
-    type' <- o A..: "type"
-    pure $
-      TableField id' name type'
+instance FromJSON Field where
+  parseJSON = A.withObject "Field" $ \o -> do
+    _field_id <- o A..: "id"
+    _field_name <- o A..: "name"
+    _field_type <- o A..: "type"
+    pure $ Field {..}
+
+-- |
+newtype Fields = Fields [Field]
+
+instance FromJSON Fields where
+  parseJSON = A.withObject "Fields" $ \o -> do
+    fieldList <- o A..: "fields"
+    pure $ Fields fieldList
 
 ------------------------------------------------------------------------------
 
 -- |
-newtype TableFields = TableFields [TableField]
+newtype ViewId = ViewId Text
+  deriving newtype FromJSON
 
-instance FromJSON TableFields where
-  parseJSON = A.withObject "TableFields" $ \o -> do
-    fields <- o A..: "fields"
-    pure $ TableFields fields
+------------------------------------------------------------------------------
+
+-- | API consumers are expected to handle unknown view names gracefully but
+-- API doesn't specify what value to fall back on, my money is on
+-- `ViewName_Grid`.
+data ViewName
+  = ViewName_Calendar
+  | ViewName_Form
+  | ViewName_Gallery
+  | ViewName_Grid
+  | ViewName_Kanban
+
+instance FromJSON ViewName where
+  parseJSON = pure . \case
+    "Calendar view" -> ViewName_Calendar
+    "Form view" -> ViewName_Form
+    "Gallery view" -> ViewName_Gallery
+    "Grid view" -> ViewName_Grid
+    "Kanban view" -> ViewName_Kanban
+    _ -> ViewName_Grid
+
+------------------------------------------------------------------------------
+
+-- | API consumers are expected to handle unknown view types gracefully but
+-- API doesn't specify what value to fall back on, my money is on
+-- `ViewType_Grid`.
+data ViewType
+  = ViewType_Calendar
+  | ViewType_Form
+  | ViewType_Gallery
+  | ViewType_Grid
+  | ViewType_Kanban
+
+instance FromJSON ViewType where
+  parseJSON = pure . \case
+    "calendar" -> ViewType_Calendar
+    "form" -> ViewType_Form
+    "gallery" -> ViewType_Gallery
+    "grid" -> ViewType_Grid
+    "kanban" -> ViewType_Kanban
+    _ -> ViewType_Grid
+
+------------------------------------------------------------------------------
+
+-- |
+data View = View
+  { _view_id :: ViewId,
+    _view_name :: ViewName,
+    _view_type :: ViewType
+  }
+
+instance FromJSON View where
+  parseJSON = A.withObject "View" $ \o -> do
+    _view_id <- o A..: "id"
+    _view_name <- o A..: "name"
+    _view_type <- o A..: "type"
+    pure $ View {..}
+
+-- |
+newtype Views = Views [View]
+
+instance FromJSON Views where
+  parseJSON = A.withObject "Views" $ \o -> do
+    viewList <- o A..: "views"
+    pure $ Views viewList
 
 ------------------------------------------------------------------------------
 
@@ -121,25 +216,24 @@ instance FromJSON TableFields where
 data Table = Table
   { _table_id :: TableId,
     _table_name :: TableName,
-    _table_primaryFieldId :: TablePrimaryFieldId,
-    _table_fields :: TableFields
+    _table_primaryFieldId :: PrimaryFieldId,
+    _table_fields :: Fields,
+    _table_views :: Views
   }
 
 instance FromJSON Table where
   parseJSON = A.withObject "Table" $ \o -> do
-    id' <- o A..: "id"
-    name <- o A..: "name"
-    primaryFieldId <- o A..: "primaryFieldId"
-    fields <- o A..: "fields"
-    pure $
-      Table id' name primaryFieldId fields
-
-------------------------------------------------------------------------------
+    _table_id <- o A..: "id"
+    _table_name <- o A..: "name"
+    _table_primaryFieldId <- o A..: "primaryFieldId"
+    _table_fields <- o A..: "fields"
+    _table_views <- o A..: "views"
+    pure $ Table {..}
 
 -- | Newtype wrapper for the response "list tables" endpoint returns.
 newtype Tables = Tables [Table]
 
 instance FromJSON Tables where
   parseJSON = A.withObject "Tables" $ \o -> do
-    tables <- o A..: "tables"
-    pure $ Tables tables
+    tableList <- o A..: "tables"
+    pure $ Tables tableList
